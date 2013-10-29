47ca6c654ae4475dddaa4914e92eb08a5917ebfa
Ribesg <ribesg@yahoo.fr>
2013-10-29 13:30:21 +0100

    Fichier Main "Hello World". A partir de ce commit, la compilation fonctionne. Utilisez "mvn clean package" à la racine du projet (au niveau du pom.xml). Le fichier compilé se trouvera dans le dossier "target". La méthode main(String[] args) de cette classe lance le constructeur de cette même classe. C'est le constructeur qui lance réellement l'application. La méthode main(String[] args) récupèrera les exceptions et les affichera proprement dans une fenêtre.

---
+++ src/main/java/fr/ribesg/imag/moustacheuml/Main.java
@@ -0,0 +1,75 @@
+package fr.ribesg.imag.moustacheuml;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+/**
+ * @author Ribesg
+ */
+public class Main {
+
+	/**
+	 * Cette méthode est le point d'entrée du programme.
+	 * Elle lance le programme en appelant {@link #Main(String[])} et
+	 * s'occupe de récupérer toutes les erreurs possibles et de les afficher.
+	 *
+	 * @param args les arguments passés au programme
+	 */
+	public static void main(String[] args) {
+		try {
+			new Main(args);
+		} catch (final Throwable t) {
+			// On affiche toute exception entrainant la fin du programme.
+			JDialog errorDialog = new JDialog(new JFrame(), "MoustacheUML - Erreur Fatale");
+
+			StringWriter sw = new StringWriter();
+			PrintWriter pw = new PrintWriter(sw);
+			t.printStackTrace(pw);
+			String errorStackTrace = sw.toString();
+
+			JTextPane errorText = new JTextPane();
+			errorText.setText(errorStackTrace);
+			errorText.setEditable(false);
+
+			errorDialog.add(errorText);
+			errorDialog.pack();
+			errorDialog.setVisible(true);
+			errorDialog.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+			errorDialog.addWindowListener(new WindowAdapter() {
+				@Override
+				public void windowClosed(WindowEvent e) {
+					System.exit(0);
+				}
+			});
+		}
+	}
+
+	/**
+	 * Le constructeur de la classe Main.
+	 * C'est ici que le programme démarre.
+	 *
+	 * @param args les arguments passés au programme
+	 */
+	public Main(String[] args) {
+		// Ouvre une fenêtre avec un Hello World
+		JFrame mainFrame = new JFrame("MoustacheUML");
+		JLabel helloWorldLabel = new JLabel("Hello wolrd!");
+
+		mainFrame.add(helloWorldLabel);
+
+		mainFrame.setMinimumSize(new Dimension(640, 480));
+		mainFrame.pack();
+		mainFrame.setVisible(true);
+		mainFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		mainFrame.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosed(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+	}
+}
